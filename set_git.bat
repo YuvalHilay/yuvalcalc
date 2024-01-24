63556c7009c02ddcfe39d9516df464067c8c8e67
unknown <yuvalos222@gmail.com>
2024-01-24 18:13:21 +0200

    first commit

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,4 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin git remote add origin https://github.com/YuvalHilay/yuvalcalc.git
-git push -u origin main
+git remote add origin git remote add origin https://github.com/YuvalHilay/ProCalc.git
